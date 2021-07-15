2db2418043be7029f9ee382e29494ec79affb369
Hyungsuk Kim <76229107+sesame0717@users.noreply.github.com>
2021-07-15 14:00:14 -0700

    updated dropdown

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-let css = document.querySelector("h3");
+let h3 = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
+let direction = document.querySelector(".direction");
 let body = document.getElementById("gradient");
 
+h3.textContent = body.style.background = "linear-gradient(to right, rgb(0, 255, 0),"
++ " rgb(255, 0, 0));";
 // console.log(css);
 // console.log(color1);
 // console.log(color2);
 
 function setGradient () {
-    body.style.background = "linear-gradient(to right, " +
+    body.style.background = "linear-gradient(" + 
+    direction.value + ", " +
     color1.value + ", " +
     color2.value + ")";
+    setText();
 }
 
+function setText () {
+    h3.textContent = body.style.background + ";"
+}
+
+direction.addEventListener("change", setGradient);
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
